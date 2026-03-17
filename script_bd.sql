e2301a81646ee13859daeb6491612481dcfe9191
pdr14444 <pedrovictorevang@gmail.com>
2026-03-17 09:44:54 -0300

    Update script_bd.sql

--- script_bd.sql
+++ script_bd.sql
@@ -78,3 +78,56 @@ select * from funcionario where dt_nasc>='2010-01-01' and dt_nasc<'2011-01-01';
 select * from funcionario where dt_nasc between '2010-01-01' and '2010-12-31';
 --FORMA 3(usando 'extract'):
 select * from funcionario where extract(year from dt_nasc) = 2010;
+
+--QUESTÃO (10/03/2026)
+--A) DATA DA PRIMEIRA HOSPEDAGEM DO HOTEL:
+select min(dt_ent) from hospedagem;
+
+--B) DATA DE NASCIMENTO DO HÓSPEDE MAIS VELHO:
+select min(dt_nasc) from hospede;
+
+--C) DATA DE NASCIMENTO DO HÓSPEDE MAIS NOVO:
+select max(dt_nasc) from hospede;
+
+--D) MÉDIA DOS VALORES DAS DIÁRIAS DE TODAS AS CATEGORIAS:
+select avg(valor_dia) from categoria;
+
+--E) MAIOR CÓDIGO DOS HÓSPEDES QUE NASCERAM EM 2010:
+select max(cod_hosp) from hospede where extract(year from dt_nasc) = 2010;
+
+--F) MENOR NÚMERO DOS APARTAMENTOS DE CATEGORIA 1:
+select min(num) from apto where cod_cat = 1;
+
+--G) NOME DO HÓSPEDE MAIS VELHO:
+select nome from hospede where dt_nasc = (select min(dt_nasc) from hospedagem);
+
+--H) CÓDIGO DO HOSPEDE E CÓDIGO DO FUNCIONÁRIO DA HOSPEDAGEM MAIS RECENTE:
+select cod_hosp,cod_func from hospedagem where dt_ent = (select max(dt_ent) from hospedagem);
+
+--I) NOME DA CATEGORIA DE MAIOR VALOR DA DIÁRIA:
+select nome from categoria where valor_dia = (select max(valor_dia) from categoria);
+
+--J) NOME DA CATEGORIA QUE O VALOR DA DIÁRIA É MAIOR QUE A MÉDIA DOS VALORES DA DIÁRIA DE TODAS AS CATEGORIAS:
+select nome from categoria where valor_dia > (select avg(valor_dia) from categoria);
+
+--EXEMPLOS: SUB-QUERY(sub-pesquisa) (17/03/2026)
+--1. NÚMERO DOS APARTAMENTOS DA CATEGORIA DE NOME LUXO:
+select num from apto where cod_cat in (select cod_cat from categoria where nome = 'wow');
+
+--2. NOME DOS HÓSPEDES QUE SE HOSPEDARAM NO ANO DE 2020:
+select nome from hospede where cod_hosp in (select cod_hosp from hospedagem where extract(year from dt_ent) = 2020);
+
+--3. NOME DOS FUNCIONÁRIOS QUE ATENDERAM O HÓSPEDE DE CÓDIGO 1:
+select nome from funcionario where cod_func in (select cod_func from hospedagem where cod_hosp = 1);
+
+--4. NOME DOS HÓSPEDES QUE FORAM ATENDIDOS PELO FUNCIONÁRIO DE NOME JOÃO:
+select nome from hospede where cod_hosp in (select cod_hosp from hospedagem where cod_func in (select cod_func from funcionario where nome = 'João'));
+
+--5. OBTER O NOME DOS HÓSPEDES QUE SE HOSPEDARAM EM APARTAMENTOS DA CATEGORIA DE CÓDIGO 1:
+select nome from hospede where cod_hosp in (select cod_hosp from hospedagem where num in (select num from apto where cod_cat = 1));
+
+--6. NOME DAS CATEGORIAS DOS APARTAMENTOS OCUPADOS PELOS HÓSPEDES QUE NASCERAM NO MÊS DE SETEMBRO:
+select nome from categoria where cod_cat in (select cod_cat from apto where num in (select num from hospedagem where cod_hosp in (select cod_hosp from hospede where extract (month from dt_nasc) = 09)));
+
+--7. CÓDIGO DAS HOSPEDAGENS REALIZADAS PARA O HÓSPEDE DE CÓDIGO 1 OU PELO FUNCIONÁRIO DE NOME JOÃO
+select cod_hospeda from hospedagem where cod_hosp = 1 or cod_func in (select cod_func from funcionario where nome = 'João');
